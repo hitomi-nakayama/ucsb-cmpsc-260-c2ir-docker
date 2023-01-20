11cc8ed14ad8014d3622244a9b0be6a922681ba1
Hitomi Nakayama <hitominakayama1.618@gmail.com>
2023-01-19 20:14:48 -0800

    added batch processing to run-c2ir.sh

--- run-c2ir.sh
+++ run-c2ir.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-set -e -x
 
-for f in "$@"; do
-    directory=$(dirname $(realpath "$f"))
+# group files by directory so we can process them in one docker run
+declare -A directories
+for f in "$@" ; do
     filename=$(basename "$f")
-    docker run --rm -u $(id -u):$(id -g) -v "${directory}:/target" -w /target c2ir "${filename}"
+    if [[ ! -v directories[@] ]] ; then
+        directory=$(dirname $(realpath "$f"))
+        directories["$directory"]="${filename}"
+    else
+        files="${directories["$directory"]}"$'\n'"${filename}"
+        directories["$directory"]="${files}"
+    fi
+done
+
+for directory in "${!directories[@]}" ; do
+    readarray -t files <<< "${directories[$directory]}"
+
+    docker run --rm -u $(id -u):$(id -g) -v "${directory}:/target" -w /target c2ir "${files[@]}"
 done
